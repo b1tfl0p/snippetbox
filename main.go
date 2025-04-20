05c07bb930222e8ca3506133df8f2409cf115fcd
b1tfl0p <b1tfl0p@proton.me>
2025-04-19 20:57:43 -0700

    Customize HTTP headers

--- main.go
+++ main.go
@@ -1,35 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-/* Three ESSENTIALS for a web app:
-* 1. Functions (Handlers) that write HTTP response headers & bodies
-* 2. A router to map paths to function handlers
-* 3. A web server to listen for incoming requests to hand to the router
- */
-
-// 1. A "home" handler function.
-// Responsible for executing your application logic and for writing HTTP
-// response headers and bodies.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current URL path exactly matches "/". If it doesn't, use the
+	// http.NotFound() function to send a 404 response.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
-	// 2. http.NewServeMux() function initializes a server multiplexer.
-	// Use the servemux to register URL patterns with the function you want to
-	// use to handle the requests to that path
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// 3. The web server
-	// Use the http.ListenAndServe() function to start a new web server.
-	// The parameters determine:
-	// * the TCP network address to listen on
-	// * the function (router) to use to handle requests
 	port := ":4000"
 	log.Printf("Starting server on %s", port)
 	err := http.ListenAndServe(port, mux)
