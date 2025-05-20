c0e9ae02646953a0f316118f6544b2ad6aa06bea
b1tfl0p <b1tfl0p@proton.me>
2025-05-20 15:09:31 -0700

    Persist valid new passwords to the database

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -288,4 +288,25 @@ func (app *application) accountPasswordUpdatePost(w http.ResponseWriter, r *http
 		app.render(w, http.StatusUnprocessableEntity, "password.gotmpl", data)
 		return
 	}
+
+	userID := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+
+	err = app.users.PasswordUpdate(userID, form.CurrentPassword, form.NewPassword)
+	if err != nil {
+		if errors.Is(err, models.ErrInvalidCredentials) {
+			form.AddFieldError("currentPassword", "Current password is incorrect")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+
+			app.render(w, http.StatusUnprocessableEntity, "password.gotmpl", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your password has been successfully changed")
+
+	http.Redirect(w, r, "/account/view", http.StatusSeeOther)
 }
